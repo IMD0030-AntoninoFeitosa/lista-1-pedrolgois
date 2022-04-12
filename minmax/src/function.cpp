436ab25ab8ffb04501324c4f7f74fb72d3d04d6b
Unknown <pedro.gois.086@A304-08L.imd.local>
2022-04-12 19:54:35 -0300

    restante das atividades

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -1,19 +1,28 @@
 #include "function.h"
 
-/*! 
- * Finds and returns a pair with the first instance of the smallest element
- * and the last instance of the largest element in an array.
- *
- * @param V This is the array-to-pointer decay representing an array.
- * @param n The array's size.
- *
- * @return A pair of indexes to the first smallest and last largest values.
- */
-
 std::pair<int,int> min_max( int V[], size_t n )
 {
-    // TODO: Adicione aqui sua solução.
+    if(n == 0){
+        return std::make_pair(-1, -1);
+    } 
 
-    // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+    int min = V[0]; 
+    std::size_t indexMin = 0;
+
+    int max = V[0]; 
+    std::size_t indexMax = 0;
+
+    for(std::size_t index = 0; index < n; index++) {
+        if(V[index] < min) {
+            min = V[index];
+            indexMin = index;
+        }
+        
+        if(V[index] >= max) { 
+            max = V[index];
+            indexMax = index;
+        } 
+    }
+    
+    return std::make_pair(minIndex, maxIndex); 
 }
--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -1,16 +1,28 @@
-/*!
- * @brief This code implements the "Soma Vizinhos" programming problem
- * @author selan
- * @data June, 6th 2021
- */
 #include <iostream>
 using std::cout;
 using std::cin;
 using std::endl;
 
-int main( void )
-{
-    // TODO: Adicione seu código aqui. 
+int main( void ) {
+    int x = 0, y = 0, soma = 0, nInicial = 0;
+    while(std::cin >> x >> y) {
+        nInicial = x;
+        soma = 0;
 
+        if(y > 0) {  
+            for(int count = 0; count < y; count++) {
+                soma += nInicial;
+                nInicial++;
+            }
+        } else if(y < 0) { 
+            int absn = y * (-1); 
+            for(int count = 0; count < absn; count++) {
+                soma += nInicial;
+                nInicial--;
+            }
+        } else soma = x; 
+
+        std::cout << soma << std::endl;
+    }
     return 0;
 }
